7bb7e5517b58065d29abd15bf4f56649981a6448
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-10-12 13:54:54 +0530

    adding router to the project

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./TodoApp.css";
 
 class TodoApp extends React.Component {
   render() {
     return (
       <div className="TodoApp">
-        <LoginComponent />
+        <Router>
+          <Switch>
+            <Route path="/" exact component={LoginComponent} />
+            <Route path="/login" component={LoginComponent} />
+            <Route path="/welcome/:name" component={WelcomeComponent} />
+            <Route component={ErrorComponent} />
+          </Switch>
+        </Router>
+        {/* <LoginComponent />
+        <WelcomeComponent /> */}
       </div>
     );
   }
+}
+
+class WelcomeComponent extends React.Component {
+  render() {
+    return <div>Welcome {this.props.match.params.name}</div>;
+  }
+}
+
+function ErrorComponent() {
+  return (
+    <div>An Error Occurred. I don't know what to do! Contact support!</div>
+  );
 }
 
 class LoginComponent extends React.Component {
@@ -41,6 +63,7 @@ class LoginComponent extends React.Component {
       this.state.password === "linkinspark"
     ) {
       console.log("Successful!");
+      this.props.history.push(`/welcome/${this.state.username}`);
       this.setState({
         hasLoginFailed: false,
         showSuccessMessage: true,
@@ -94,22 +117,6 @@ class LoginComponent extends React.Component {
         <button onClick={this.loginClicked}>Login</button>
       </div>
     );
-  }
-}
-
-function ShowInvalidCredentials(props) {
-  if (props.hasLoginFailed) {
-    return <div>Invalid Credentials</div>;
-  } else {
-    return null;
-  }
-}
-
-function ShowLoginSuccessMessage(props) {
-  if (props.showSuccessMessage) {
-    return <div>Login sucessful</div>;
-  } else {
-    return null;
   }
 }
 
