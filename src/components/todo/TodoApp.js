4c99e7ed81dac28259d06875371d06a0719bc072
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-10-12 11:38:17 +0530

    adding states for form elements

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -12,11 +12,49 @@ class TodoApp extends React.Component {
 }
 
 class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "in28minutes",
+      password: "",
+    };
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    console.log("Username>>> ", event.target.value);
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handlePasswordChange(event) {
+    console.log("Password>>> ", event.target.value);
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
-        User Name: <input type="text" name="username" />
-        Password: <input type="password" name="password" />
+        User Name:{" "}
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
         <button>Login</button>
       </div>
     );
