4844954f37c85aaf4ebcbec7bc6a6ff85539ca4b
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-11-18 22:37:37 +0530

    adding moment and formik into the project for updating the todo

--- src/components/todo/TodoComponent.js
+++ src/components/todo/TodoComponent.js
@@ -1,8 +1,49 @@
 import React from "react";
+import moment from "moment";
+import { Field, Form, Formik } from "formik";
 
 class TodoComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 1,
+      description: "Learn Forms",
+      targetDate: moment(new Date()).format("YYYY-MM-DD"),
+    };
+  }
   render() {
-    return <div>Todo Component for id {this.props.match.params.id}</div>;
+    return (
+      <div>
+        <h1>Todo</h1>
+        <div className="container">
+          <Formik>
+            {(props) => (
+              <Form>
+                <fieldset className="form-group">
+                  <label>Description</label>
+                  <Field
+                    className="form-control"
+                    type="text"
+                    name="description"
+                  ></Field>
+                </fieldset>
+                <fieldset className="form-group">
+                  <label>Target Date</label>
+                  <Field
+                    className="form-control"
+                    type="date"
+                    name="date"
+                  ></Field>
+                </fieldset>
+                <button className="btn btn-success" type="submit">
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    );
   }
 }
 
